822a1d8f6d63678ea924b5e19d6381f0dbe4f755
Dylan Marco <dylan.marco.sala@gmail.com>
2024-12-02 13:15:23 +0100

    .

---
+++ DWC/Tema5/Practica2/Reto0/scripts/script.js
@@ -0,0 +1,7 @@
+let div = document.getElementsByTagName("div")[0];
+div.style.width = "30%"
+div.style.height = "250px"
+div.style.borderTop = "5px dashed #00FF00"
+div.style.paddingLeft = "15px"
+div.style.marginBottom = "25px"
+div.style.color = "red"
---
+++ DWC/Tema5/Practica2/Reto2/scripts/sctipt.js
@@ -0,0 +1,3 @@
+function leerParrafo() {
+    document.addEventListener
+}
